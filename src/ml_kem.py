cb347eac799068e670fb92723b533299bb9a2d3e
Franco Ruiz <francoruiz287@gmail.com>
2024-11-27 21:03:27 -0600

    comentarios añadidos

--- src/ml_kem.py
+++ src/ml_kem.py
@@ -4,33 +4,32 @@ import numpy as np
 from kyber import Kyber512
 
 def prueba_rendimiento_ml_kem(iteraciones=100):
-    """Realiza pruebas de rendimiento para Kyber512."""
     try:
-        # Listas para almacenar los tiempos
+        #Listas para almacenar los tiempos
         tiempos_encapsulamiento = []
         tiempos_desencapsulamiento = []
 
         for _ in range(iteraciones):
-            # Generar claves pública y privada
+            #Generacion de claves pública y privada
             clave_publica, clave_privada = Kyber512.keygen()
 
-            # Medir tiempo de encapsulamiento
+            #Medición del tiempo de encapsulamiento
             inicio = time.time()
             ciphertext, secreto_compartido_emisor = Kyber512.enc(clave_publica)
             tiempo_encapsulamiento = time.time() - inicio
             tiempos_encapsulamiento.append(tiempo_encapsulamiento)
 
-            # Medir tiempo de desencapsulamiento
+            #Medición del tiempo de desencapsulamiento
             inicio = time.time()
             secreto_compartido_receptor = Kyber512.dec(ciphertext, clave_privada)
             tiempo_desencapsulamiento = time.time() - inicio
             tiempos_desencapsulamiento.append(tiempo_desencapsulamiento)
 
-            # Verificar que los secretos compartidos coincidan
+            #Verificacion de que los secretos compartidos coincidan
             if secreto_compartido_emisor != secreto_compartido_receptor:
                 raise ValueError("No hay coincidencia")
 
-        # Calcular estadísticas de rendimiento
+        #Calculo de estadísticas de rendimiento
         promedio_encapsulamiento = np.mean(tiempos_encapsulamiento)
         desviacion_encapsulamiento = np.std(tiempos_encapsulamiento)
         promedio_desencapsulamiento = np.mean(tiempos_desencapsulamiento)
